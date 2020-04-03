03b6759da16d1b60addea152608d93d23b2e4a0b
RipFive <richard.poelderl@gmail.com>
2020-04-03 12:41:42 +0200

    Solution one passing all 9 tests on HR

---
+++ solution.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import os
+import sys
+
+inp = '12:05:45AM'
+
+def timeConversion(s):
+    s.strip()
+    is_pm = True if inp[-2:].lower() == 'pm' else False
+    hh_mm_ss_int_list = list(map(int, inp[:-2].split(':')))
+    if is_pm and hh_mm_ss_int_list[0] < 12:
+        hh_mm_ss_int_list[0] += 12
+    elif not is_pm:
+        print('I''m here')
+        hh_mm_ss_int_list[0] -= 12 if hh_mm_ss_int_list[0] == 12 else 0
+
+    hh_mm_ss_str_list = list(map(lambda el: f"{el:02d}", hh_mm_ss_int_list))
+    print(':'.join(hh_mm_ss_str_list))
+    
+timeConversion(inp)
