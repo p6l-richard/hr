eca29d4549fc5ff3e0a318d7ea8b5b304f7a42f7
p6l-richard <richard.poelder@gmail.com>
2020-04-22 09:12:11 +0200

    init problem

--- simple-array-sum/solution.py
+++ simple-array-sum/solution.py
@@ -1,8 +1,5 @@
 #!/bin/python3
 
-import os
-import sys
-
 #
 # Complete the simpleArraySum function below.
 #
@@ -10,16 +7,4 @@ def simpleArraySum(ar):
     #
     # Write your code here.
     #
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    ar_count = int(input())
-
-    ar = list(map(int, input().rstrip().split()))
-
-    result = simpleArraySum(ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+result = simpleArraySum(ar)
